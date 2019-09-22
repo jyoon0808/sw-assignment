40f5b896ea50d604beb632016409c41defb61d64
Jun Young Yoon <jyoon@connet.ust.hk>
2019-09-22 23:02:26 +0800

    jyoon first commit

--- assignment1/implementation.c
+++ assignment1/implementation.c
@@ -3,7 +3,53 @@
 
 Result getLargestConsecutiveChar(char *str)
 {
+    char string1[100], visited[100];
+    int count[100] = {0}, flag = 0;
+    
     Result r;
-    // implementation here
+    int i, j = 0, k = 0, l, max, index;
+ 
+    l = strlen(str);
+ 
+    for (i = 0; i < l; i++)
+    {
+        if (i == 0)
+        {
+            visited[j++] = string1[i];
+            count[j - 1]++;
+        }
+        else
+        {
+            for (k = 0; k  < j; k++)
+            {
+                if (string1[i] == visited[k])
+                {
+                    count[k]++;
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                visited[j++] = string1[i];
+                count[j - 1]++;
+            }
+            flag = 0;
+        }
+    }    
+ 
+    for (i = 0; i < j; i++)
+    {
+        if ((i == 0) && (visited[i] != ' '))
+        {
+            max = count[i];
+            continue;
+        }
+        if ((max < count[i]) && (visited[i] != ' '))
+        {
+            max = count[i];
+            index = i;
+            r = index;
+        }
+    }
     return r;
 }
